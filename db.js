75f976cbcfaeedfcb4f5334ad176168dd5ad926e
Tom <tommeyers.tm127@gmail.com>
2018-02-03 22:08:41 +0100

    Full Language List (Just To Remember xD)

--- db.js
+++ db.js
@@ -6,24 +6,104 @@ var version = {
 };
 
 var content = [
-  {
-    name: "HTML",
-    path: "langs/html.json",
-    prism: "lang-html"
-  },
-  {
-    name: "CSS",
-    path: "langs/css.json",
-    prism: "lang-scss"
-  },
-  {
-    name: "JS",
-    path: "langs/js.json",
-    prism: "lang-js"
-  },
-  {
-    name: "C++",
-    path: "langs/cpp.json",
-    prism: "lang-cpp"
-  }
+        {
+            name: "C++",
+            path: "langs/Lang/cpp.json",
+            prism: "language-cpp"
+        },
+        {
+            name: "C#",
+            path: "langs/Lang/cs.json",
+            prism: "language-csharp"
+        },
+        {
+            name: "JAVA",
+            path: "langs/Lang/java.json",
+            prism: "language-java"
+        },
+        {
+            name: "PY",
+            path: "langs/Lang/py.json",
+            prism: "language-python"
+        },
+        {
+            name: "ARD",
+            path: "langs/Lang/ard.json",
+            prism: "language-arduino"
+        },
+        {
+            name: "SH",
+            path: "langs/Lang/sh.json",
+            prism: "language-bash"
+        },
+        {
+            name: "GL",
+            path: "langs/Api/gl.json",
+            prism: "language-cpp"
+        },
+        {
+            name: "UE4",
+            path: "langs/Api/ue4.json",
+            prism: "language-cpp"
+        },
+        {
+            name: "UNITY",
+            path: "langs/Api/unity.json",
+            prism: "language-csharp"
+        },
+        {
+            name: "DP",
+            path: "langs/Etc/dp.json",
+            prism: "language-cpp"
+        },
+        {
+            name: "GIT",
+            path: "langs/Etc/git.json",
+            prism: "language-git"
+        },
+        {
+            name: "MOBILE",
+            path: "langs/Etc/mobile.json",
+            prism: "language-java"
+        },
+        {
+            name: "US",
+            path: "langs/Etc/US.json",
+            prism: "lang-html"
+        },
+        {
+            name: "JSON",
+            path: "langs/Data/json.json",
+            prism: "lang-html"
+        },
+        {
+            name: "XML",
+            path: "langs/Data/xml.json",
+            prism: "lang-html"
+        },
+        {
+            name: "XAML",
+            path: "langs/Data/wpf.json",
+            prism: "lang-html"
+        },
+        {
+          name: "HTML",
+          path: "langs/Web/html.json",
+          prism: "lang-html"
+        },
+        {
+          name: "CSS",
+          path: "langs/Web/css.json",
+          prism: "lang-scss"
+        },
+        {
+          name: "JS",
+          path: "langs/Web/js.json",
+          prism: "language-js"
+        },
+        {
+            name: "TS",
+            path: "langs/Web/ts.json",
+            prism: "language-typescript"
+        }
 ];
