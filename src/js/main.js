71a319ad02fe3a8d2d17332d955f820a1699c152
Matthias Willemsen <me@mattwill.be>
2018-02-04 14:37:45 +0100

    Added a sticky navigation bar

--- src/js/main.js
+++ src/js/main.js
@@ -23,10 +23,32 @@ var app = new Vue({
           document.getElementById('navigation').style.display = 'grid';
         }
         this.navbarState = !this.navbarState;
+      } else {
+        // TODO: Handle forced
+      }
+    },
+    handleScroll: function () {
+      var header = document.getElementById('header').style;
+      var holder = document.getElementById('header-holder').style;
+      var hero = document.getElementById('title-large').style;
+      if (window.scrollY > 180) {
+        header.height = '76px';
+        header.position = 'fixed';
+        holder.display = 'block';
+        hero.lineHeight = '76px';
+        hero.fontSize = '24px';
+      } else {
+        header.height = '256px';
+        header.position = 'relative';
+        holder.display = 'none';
+        hero.display = 'inline-block';
+        hero.lineHeight = '320px';
+        hero.fontSize = '56px';
       }
     }
   },
   mounted: function(){
+    window.addEventListener('scroll', this.handleScroll);
     content.forEach(function (lang, index) {
       list.push(
         fetch(lang.path).then(function (res) {
