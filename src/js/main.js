44c7da56d42efedb7f2c10e7a6f5268ad9b183df
Matthias Willemsen <me@mattwill.be>
2018-02-05 14:24:01 +0100

    Removed unused code

--- src/js/main.js
+++ src/js/main.js
@@ -67,55 +67,6 @@ var app = new Vue({
   }
 });
 
-var x = {
-  mounted: function(){
-    window.addEventListener('scroll', this.handleScroll);
-    content.forEach(function (lang, index) {
-      list.push(
-        fetch(lang.path).then(function (res) {
-          return res.json();
-        }).then(function (value) {
-          results[lang.name] = value;
-        })
-      )
-    });
-    Promise.all(list).then(function () {
-      console.log('Loaded External JSON Files.');
-      console.log('Total amount: ' + list.length);
-      this.elements = results;
-      app.activePage = 'C++';
-      setTimeout(function () {
-        Prism.highlightAll();
-      }, 10);
-    });
-  }
-};
-var y = {
-  y: function () {
-    keys.forEach(function (subdev) {
-      modules[subdev].contains.forEach(function (lang) {
-        list.push(
-          fetch(modules.langs[lang].path).then(function (res) {
-            return res.json();
-          }).then(function (value) {
-            list[lang] = value;
-          })
-        )
-      });
-    });
-    Promise.all(list).then(function () {
-      console.log('Loaded External JSON Files.');
-      console.log('Total amount: ' + list.length);
-      allPageContent = list;
-      app.activePage = 'CPP';
-
-      setTimeout(function () {
-        Prism.highlightAll();
-      }, 10);
-    });
-  }
-};
-
 console.log('------- DEV START -------');
 console.log(app.allLangs);
 console.log(app.allLangsSections);
